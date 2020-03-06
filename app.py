173dc1de74a4dd9b2a600ebeb57f7effe2d0e641
NefretySanchez <57744620+NefretySanchez@users.noreply.github.com>
2020-03-06 17:48:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_redis import Redis
 
 app= Flask(__name__)
+app.config['REDIS_HOST'] = 'localhost'
+app.config['REDIS_PORT'] = 6379
+app.config['REDIS_DB'] = 0
 redis= Redis(app)
 
 if __name__== '__main__':
