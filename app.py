6e4258d2fb21b97d44d8c38ddee3143c84c1b4cc
NefretySanchez <57744620+NefretySanchez@users.noreply.github.com>
2020-03-06 15:33:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from flask_redis import Redis
 
 app= Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Holaa'
+redis= Redis(app)
 
 if __name__== '__main__':
     app.run(debug=True)
